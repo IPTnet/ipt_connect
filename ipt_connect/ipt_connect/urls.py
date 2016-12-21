87a2b3ba6cfd4b1421b4a17165a9b896d98aa6ad
mhrz <maxime.harazi@normale.fr>
2016-12-21 22:40:18 +0100

    difference between github and the server

--- ipt_connect/ipt_connect/urls.py
+++ ipt_connect/ipt_connect/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from views import home
 
 urlpatterns = [
     # Examples:
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-    url(r'^$', home),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),#'ipt_connect.views.home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^IPT2016/', include('IPT2016.urls', namespace='IPT2016')),
     url(r'^FPT2017/', include('FPT2017.urls', namespace='FPT2017')),
