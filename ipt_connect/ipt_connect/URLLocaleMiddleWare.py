303f45fa61c30207c1ae64c085fd50600bbc27b3
mhrz <maxime.harazi@normale.fr>
2017-01-26 16:18:24 +0100

    Automatic translations of internals
    Don't forget to add ipt_connect.URLLocaleMiddleWare.URLLocaleMiddleware
    in the middleware of settings.py

---
+++ ipt_connect/ipt_connect/URLLocaleMiddleWare.py
@@ -0,0 +1,19 @@
+from django.utils import translation
+
+url_locale = (('/FPT2017','fr'),)
+
+
+class URLLocaleMiddleware:
+
+    def process_request(self, request):
+        for (url,loc) in url_locale:
+            if request.path.startswith(url):
+                        request.LANG = loc
+                        translation.activate(request.LANG)
+                        request.LANGUAGE_CODE = request.LANG
+
+
+
+    def process_response(self, request, response):
+        translation.deactivate()
+        return response
