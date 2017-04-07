e2be9c78f010cbed7a3014b48bf7915faf9e6160
mhrz <maxime.harazi@normale.fr>
2017-04-07 11:01:00 +0200

    Adding the possibility not to give email to "Jury" instance.
    Used for TLs which are not Jurors.

--- ipt_connect/IPT2017/models.py
+++ ipt_connect/IPT2017/models.py
@@ -397,7 +397,7 @@ class Jury(models.Model):
 		:return: return the full name of the jury member
 		"""
 		return self.name+' '+self.surname
-	email = models.EmailField(help_text='This address will be used to send the participant every important infos about the tournament.',verbose_name='Email')
+	email = models.EmailField(help_text='This address will be used to send the participant every important infos about the tournament.',verbose_name='Email', blank=True)
 	affiliation = models.CharField(max_length=100,blank=True,verbose_name='Affiliation to display',help_text='Will be used for export (badges and web).')
 	team = models.ForeignKey('Team', null=True, blank=True)
 	pf1 = models.BooleanField(default=False,verbose_name='PF 1')
@@ -611,7 +611,7 @@ class EternalRejection(models.Model):
 # method for updating Teams and Participants when rounds are saved
 @receiver(post_save, sender=Round, dispatch_uid="update_participant_team_points")
 def update_points(sender, instance, **kwargs):
-	print "Updating points !!!"
+	print "YOLOOOO !!!"
 	if (instance.reporter_team is None) or (instance.opponent_team is None) or (instance.reviewer_team is None) or instance.problem_presented is None :
 		# then all teams aren't yet defined, there is no need to compute scores
 		pass
