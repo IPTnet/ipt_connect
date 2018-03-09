82693f938183a988414aa968f5a7e7fda80998b6
mhrz <maxime.harazi@normale.fr>
2018-03-09 20:43:54 +0100

    update homepage

--- ipt_connect/ipt_connect/urls.py
+++ ipt_connect/ipt_connect/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
+from ipt_connect.views import home
+from IPT2018.views import tournament_overview
 
 urlpatterns = [
     # Examples:
@@ -9,12 +10,14 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-    url(r'^$', TemplateView.as_view(template_name='index.html')),#'ipt_connect.views.home'),
+    #url(r'^$', home, name='home'), #TemplateView.as_view(template_name='index.html')),#'ipt_connect.views.home'),
+	url(r'^$', tournament_overview),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', include('loginas.urls')),
     url(r'^IPT2016/', include('IPT2016.urls', namespace='IPT2016')),
     url(r'^FPT2017/', include('FPT2017.urls', namespace='FPT2017')),
 	url(r'^IPT2017/', include('IPT2017.urls', namespace='IPT2017')),
+	url(r'^IPT2018/', include('IPT2018.urls', namespace='IPT2018')),
 ]
 
 
