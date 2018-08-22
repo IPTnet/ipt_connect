98ed0af33846f7310010f4d268f9a2be7a5c4218
Sergey Baboshin <ijustbsd@gmail.com>
2018-08-22 13:26:36 +0300

    Tournament name was changed

--- ipt_connect/IPTdev/parameters.py
+++ ipt_connect/IPTdev/parameters.py
@@ -2,7 +2,10 @@
 
 # Views parameters
 app_version = "dev"     # keyword for url parsing
-NAME = "IPT dev"
+NAME = {
+    'short': 'IPT 2019',
+    'full': 'International Physicists\' Tournament 2019'
+}
 
 # Models parameters
 npf = 4                 # Number of Physics fights
@@ -13,7 +16,7 @@ netreject_max = 1       # Maximum number of eternal rejection
 
 # Personal ranking
 personal_ranking = {
-    'active': True,
+    'active': False,
     'rep_threshold': 5,
     'opp_threshold': 5,
     'rev_threshold': 5,
