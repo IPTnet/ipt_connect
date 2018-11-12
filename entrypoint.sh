0a18a05d72e179c7e302bbcfebce258874e0eb42
Sergey Baboshin <ijustbsd@gmail.com>
2018-11-12 10:34:13 +0300

    Added docker-compose for nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!bin/sh
 cd /ipt_connect
-python manage.py runserver 0.0.0.0:8000
+
+if [ "$DEV" == "true" ]; then
+    python manage.py runserver 0.0.0.0:8000
+    exit 1
+fi
+
+gunicorn --workers=${WORKERS} --bind=unix:/ipt_connect/ipt_connect.sock ipt_connect.wsgi
