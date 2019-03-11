c6d446dbda5a68c9b5e7a47bfb16c1bb8daef59b
NickKolok <nickkolok@mail.ru>
2019-03-11 18:49:07 +0300

    Dirty hack and TODO note to allow automated cloning

--- ipt_connect/IPTdev/parameters.py
+++ ipt_connect/IPTdev/parameters.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Views parameters
-app_version = "dev"     # keyword for url parsing
+app_version = "IPTdev"[3:]     # keyword for url parsing
+# TODO:
+# The construction `"IPTdev"[3:]` above is a dirty hack.
+# We need to mention `IPTdev` to enable automated cloning,
+# but we have to set version to `dev` to satisfy some pieces of code.
+# No idea how is it handled for FPT.
+# To be refactored:
+# grep "app_version"
+
+
 NAME = {
     'short': 'IPT 2019',
     'full': 'International Physicists\' Tournament 2019'
