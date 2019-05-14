270992c56bb5177615dcedc92cccf12e3f35fd11
NickKolok <nickkolok@mail.ru>
2019-05-14 20:56:32 +0300

    Anybody who is staff can trigger TTH update_all - a dirty hack; waiting for #106

--- ipt_connect/IPT_TTH2019POV/views.py
+++ ipt_connect/IPT_TTH2019POV/views.py
@@ -109,7 +109,7 @@ def jury_export_web(request):
 
 	return render(request, 'IPT%s/listing_jurys_web.html' % params.app_version, {'jurys': jurys})
 
-@user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda u: u.is_staff)
 def update_all(request):
 	list_receivers = update_signal.send(sender=Round)
 
