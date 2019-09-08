a7f361b66e2e767d317723cf72688e2216b75f80
zudik <vutden1@yandex.ru>
2019-09-08 15:18:19 +0300

    Right path for dump

--- ipt_connect/IPTdev/utils/dumper.py
+++ ipt_connect/IPTdev/utils/dumper.py
@@ -4,9 +4,9 @@ from urllib2 import urlopen
 from find_links import unique_url
 
 dir_path_repo = str(os.getcwd())
-repo = git.Repo(dir_path_repo[:-12])
+repo = git.Repo(dir_path_repo[:-24])
 last_commit = str(repo.head.commit)[:7]
-dir_path_dump = dir_path_repo[:-23] + 'dump/' + last_commit
+dir_path_dump = dir_path_repo[:-24] + 'dump/' + last_commit
 
 if not os.path.exists(dir_path_dump):
         os.makedirs(dir_path_dump)
