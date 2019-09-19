cfec8c7bc2d31b211281ca0a4cf918ac12446966
NickKolok <nickkolok@mail.ru>
2019-09-19 22:29:13 +0300

    IPT_TTH2019_FINAL - naming and 4 fights instead of 3

--- ipt_connect/IPT_TTH2019_FINAL/parameters.py
+++ ipt_connect/IPT_TTH2019_FINAL/parameters.py
@@ -12,10 +12,10 @@ app_version = "IPT_TTH2019_FINAL"[3:]     # keyword for url parsing
 
 
 NAME = {
-    'short': 'ТТН 2019 Приволжье',
-    'full': 'Турнир Трёх Наук 2019 - Приволжский этап',
+    'short': 'ТТН 2019 - Финальный этап',
+    'full': 'Турнир Трёх Наук 2019 - Финальный этап',
     # ... and the name used in tournament overview
-    'front': 'Приволжский этап Федерального Студенческого Турнира Трёх Наук',
+    'front': 'Финального этапа Федерального Студенческого Турнира Трёх Наук',
 }
 
 poster_url = ''
@@ -25,7 +25,7 @@ website_url = 'http://iturnir.ru'
 repo_url = 'https://github.com/IPTnet/ipt_connect'
 
 # Models parameters
-npf = 3                 # Number of selective (qualifying) Physics fights
+npf = 4                 # Number of selective (qualifying) Physics fights
 semifinals_quantity = 0 # Quantity of semifinals. Every semifinal should be a separate fight!
 with_final_pf = True    # Is there a Final Fight ?
 reject_malus = 0.2      # Malus for too many rejections
@@ -98,22 +98,22 @@ fights = {
     # Bonus points multipliers
     # You can set it to 2 for semifinals or the last selective PFs to emphase the drama.
     # You can also set it to zero for the final with the same purpose!
-    'bonus_multipliers' : [0,0,0,0,0],
+    'bonus_multipliers' : [0,0,0,0,0,0],
 
     # Is there challenge procedure at the beginning of each round of the fight?
     # Some tournaments, such as Syberian IYPT, play the last selective PF without the challenge procedure
     # (i.e. like a Final: every team decides what to present)
     # This parameter influences (now) only on visibility of "Forbidden problems" block
     # while rendering the round detail
-    'challenge_procedure': [True, True, True, False],
+    'challenge_procedure': [True, True, True, True, False],
 
     # A situation when a team chooses a problem without challenge,
     # but there are problems which cannot be chosen, is easy to imagine
     # So, we have a separate setting for forbidden problems!
-    'problems_forbidden': [True, True, True, False],
+    'problems_forbidden': [True, True, True, True, False],
 
     # And, finally, you can specify names for all the fights to be displayed
     # Sometimes it is useful to name fights like "Day 1 - Fight 2", or "Fight By Choice",
     # or "Semifinal A", or smth another.
-    'names': ['Scientific Fight 1','Scientific Fight 2','Scientific Fight 3','The Final']
+    'names': ['Scientific Fight 1','Scientific Fight 2','Scientific Fight 3','Scientific Fight 4','The Final']
 }
