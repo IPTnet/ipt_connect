b7f3d2cb24051665449ff5ca14c59ce38491758a
NickKolok <nickkolok@mail.ru>
2019-12-12 21:22:48 +0300

    [fix] Eliminate non-unicode naming for rejections
    Otherwise, cyrillic symbols in the name of a problem
    lead to crash on saving a Round with the problem rejected.

--- ipt_connect/IPTdev/models.py
+++ ipt_connect/IPTdev/models.py
@@ -651,7 +651,7 @@ class TacticalRejection(models.Model):
 	problem = models.ForeignKey(Problem)
 
 	def __unicode__(self):
-		return "Problem rejected : %s" % self.problem
+		return "Problem rejected : %s" % self.problem.pk
 
 class EternalRejection(models.Model):
 
@@ -659,7 +659,7 @@ class EternalRejection(models.Model):
 	problem = models.ForeignKey(Problem)
 
 	def __unicode__(self):
-		return "Problem rejected : %s" % self.problem
+		return "Problem rejected : %s" % self.problem.pk
 
 
 # method for updating Teams and Participants when rounds are saved
