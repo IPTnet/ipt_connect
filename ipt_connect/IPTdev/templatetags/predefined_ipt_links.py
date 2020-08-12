9461b7786ef24c1af1a9b7d1f304a99a76acbb7a
NickKolok <nickkolok@mail.ru>
2020-08-13 00:37:22 +0300

    [refactoring] Predefined links to make the templates more laconic

---
+++ ipt_connect/IPTdev/templatetags/predefined_ipt_links.py
@@ -0,0 +1,25 @@
+from django import template
+from IPTdev.models import *
+
+register = template.Library()
+
+"""
+This file provides a convenient way to put links to the models
+into templates.
+E.g. instead of:
+	<a href="{% url 'IPTdev:team_detail' team_name=juryroundsgrade.round.reporter_team.name %}">{{juryroundsgrade.round.reporter_team.name}}</a>
+you can now write just:
+	{% team_link juryroundsgrade.round.reporter_team %}
+(don't forget to `{% load predefined_ipt_links %}` in the beginning of the template!)
+
+Please put all the models in the alphabetical order (be git-friendly!)
+
+The refactoring is not finished yet :(
+"""
+
+
+@register.inclusion_tag('IPTdev/includes/team_link.html')
+def team_link(team):
+	if isinstance(team, Team):
+		team = team.name
+	return { 'team_name' : team }
