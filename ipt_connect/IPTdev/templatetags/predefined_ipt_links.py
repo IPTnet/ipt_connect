44913ffcfa4c330e1160fb1c3ef526cff62e690e
NickKolok <nickkolok@mail.ru>
2020-08-16 00:11:48 +0300

    [refactoring][#78][docs] Replace 'IPTdev' with an expression in the example of predefined links - see also #241

--- ipt_connect/IPTdev/templatetags/predefined_ipt_links.py
+++ ipt_connect/IPTdev/templatetags/predefined_ipt_links.py
@@ -8,7 +8,7 @@ register = template.Library()
 This file provides a convenient way to put links to the models
 into templates.
 E.g. instead of:
-	<a href="{% url 'IPTdev:team_detail' team_name=juryroundsgrade.round.reporter_team.name %}">{{juryroundsgrade.round.reporter_team.name}}</a>
+	<a href="{% url params.instance_name|add:':team_detail' team_name=juryroundsgrade.round.reporter_team.name %}">{{juryroundsgrade.round.reporter_team.name}}</a>
 you can now write just:
 	{% team_link juryroundsgrade.round.reporter_team %}
 (don't forget to `{% load predefined_ipt_links %}` in the beginning of the template!)
