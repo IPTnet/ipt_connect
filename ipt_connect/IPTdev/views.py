1b4ab3e4dc651b58cff9ea1168b5c6c2626160b0
NickKolok <nickkolok@mail.ru>
2020-08-16 19:47:15 +0300

    [refactoring][#157] Generate the 'Edit' link for a Round directly inside the template

--- ipt_connect/IPTdev/views.py
+++ ipt_connect/IPTdev/views.py
@@ -502,8 +502,6 @@ def round_detail(request, pk):
 	tacticalrejections = TacticalRejection.objects.filter(round=round)
 	eternalrejection = EternalRejection.objects.filter(round=round)
 
-	link_to_change_round = '/admin/IPT' + str(params.app_version) + '/round/' + str(pk) + '/change'
-
 	return render(
 		request,
 		'IPT%s/round_detail.html' % params.app_version,
@@ -520,7 +518,6 @@ def round_detail(request, pk):
 			'display_rejections': params.fights['challenge_procedure'][round.pf_number - 1],
 			'display_problems_forbidden': params.fights['problems_forbidden'][round.pf_number - 1],
 			'physics_fight_name': params.fights['names'][round.pf_number - 1],
-			'link_to_change_round': link_to_change_round,
 		}
 	)
 
