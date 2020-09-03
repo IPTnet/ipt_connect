957dbb0c39d2371c398f1826fd679ff0daaae9a9
Vladimir <sirix.vladim@gmail.com>
2020-09-03 18:58:22 +0300

    [refactoring] Distribution bonus points for any count of teams - see #240, see #249

--- ipt_connect/IPTdev/func_bonus.py
+++ ipt_connect/IPTdev/func_bonus.py
@@ -1,63 +1,13 @@
 #Functions for calculating bonus points
 
+
 def distribute_bonus_points(points_list):
-	if len(points_list) == 3:
-		return distribute_bonus_points_3(points_list)
-	if len(points_list) == 4:
-		return distribute_bonus_points_4(points_list)
-	return [2.0, 1.0, 1.0, 1.0, 0.0]
-
-def distribute_bonus_points_3(points_list):
-
-	# If everyone is ex-aequo
-	if points_list[0] == points_list[1] and points_list[0] == points_list[2]:
-		return [1.0, 1.0, 1.0]
-
-	# If 1 and 2 are ex-aequo
-	if points_list[0] == points_list[1]:
-		return [1.5, 1.5, 0.0]
-
-	# If 2 and 3 are ex-aequo
-	if points_list[1] == points_list[2]:
-		return [2.0, 0.5, 0.5]
-
-	# If no ex-aequo
-	return [2.0, 1.0, 0.0]
-
-
-def distribute_bonus_points_4(points_list):
-
-	######################
-	# 4 teams ex-aequo
-	# If everyone is ex-aequo
-	if points_list[0] == points_list[1] and points_list[1] == points_list[2] and points_list[2] == points_list[3]:
-		return [1.0, 1.0, 1.0, 1.0]
-	######################
-
-	######################
-	# 3 teams ex-aequo
-	if points_list[0] == points_list[1] and points_list[1] == points_list[2]:
-		return [4.0/3.0, 4.0/3.0, 4.0/3.0, 0.0]
-	if points_list[1] == points_list[2] and points_list[2] == points_list[3]:
-		return [2.0, 2.0/3.0, 2.0/3.0, 2.0/3.0]
-	######################
-
-	######################
-	# 2 pairs of teams ex-aequo
-	if points_list[0] == points_list[1] and points_list[2] == points_list[3]:
-		return [1.5, 1.5, 0.5, 0.5]
-	######################
-
-	######################
-	# One pair of teams ex-aequo
-	if points_list[0] == points_list[1]:
-		return [1.5, 1.5, 1.0, 0.0]
-	if points_list[1] == points_list[2]:
-		return [2.0, 1.0, 1.0, 0.0]  # Redundant, but let it be
-	if points_list[2] == points_list[3]:
-		return [2.0, 1.0, 0.5, 0.5]
-	######################
-
-	# No ex-aequo
-	return [2.0, 1.0, 1.0, 0.0]
-
+    max_points = len(points_list)
+    # 'equal_count' is a variable that contain the count of coincident elements in 'points_list'
+    equal_count = points_list.count(points_list[0])
+    if max_points == equal_count:
+        return [1.0 for x in range(equal_count)]
+    head = [(equal_count + 1.0) / equal_count for x in range(equal_count)]
+    equal_count = points_list.count(points_list[max_points-1])
+    tail = [(equal_count - 1.0) / equal_count for x in range(equal_count)]
+    return head + [1.0 for x in range(max_points - (len(head)+len(tail)))] + tail
