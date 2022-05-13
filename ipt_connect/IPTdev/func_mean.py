0dcfc2adc3bf4e53b1d28143488ad4c654922161
NickKolok <nickkolok@mail.ru>
2022-05-13 19:22:35 +0300

    Refactoring - func_mean.py (partial) - by @fast-geek

--- ipt_connect/IPTdev/func_mean.py
+++ ipt_connect/IPTdev/func_mean.py
@@ -1,5 +1,6 @@
 # All functions for calculating mean value are independent of other models, so don't use import
 
+
 def mean(vec):
 	if len(vec) != 0:
 		return float(sum(vec)) / len(vec)
@@ -26,11 +27,13 @@ def ipt_mean(vec):
         vec = vec[nlow : -nhigh]
     return mean(vec)
 
+
 def iypt_mean(vec):
-    if len(vec) > 1 :
+    if len(vec) > 1:
         vec.append((vec.pop(0) + vec.pop()) / 2.0)
         return mean(vec)
     return mean(vec)
+
 
 def ttn_mean(vec):
     if len(vec) <= 4:
