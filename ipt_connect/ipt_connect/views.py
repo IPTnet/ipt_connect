5bc839b19fb690eeb76d0e428ecad4455dc267d2
NickKolok <nickkolok@mail.ru>
2022-05-14 00:33:40 +0300

    Code style - ipt_connect/views.py - by @fast-geek

--- ipt_connect/ipt_connect/views.py
+++ ipt_connect/ipt_connect/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
-def home(request):
 
+def home(request):
     text = """<h1>IPT Connect</h1>
 
               <a href="http://connect.iptnet.info/IPTdev">Results of IPT dev</a>"""
